3b2b982a441bfbff77c0757fde9ddc8765243edb
jacquesdanielsamuel3-cyber <jacquesdanielsamuel3@gmail.com>
2025-08-22 14:31:41 +0400

    Delete Capstone Project directory

--- Capstone Project/Update script.js for Price Calculation.js
+++
@@ -1,8 +0,0 @@
-function calculatePrice() {
-    let carType = document.getElementById('carType').value;
-    let days = parseInt(document.getElementById('days').value);
-    let pricePerDay = (carType === 'SUV') ? 50 : 40;
-    let totalPrice = pricePerDay * days;
-
-    document.getElementById('totalPrice').textContent = totalPrice;
-}
